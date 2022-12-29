9d3312aab447028cb92717fa6da463732424551d
hhh1053 <um1221@pusan.ac.kr>
2022-12-29 18:26:12 +0900

    first commit

---
+++ src/test/java/com/carenco/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carenco.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
